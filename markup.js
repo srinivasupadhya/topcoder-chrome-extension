327719687e3fe8b776d167e37bc424d51365b7af
Srini <srinivas.upadhya@gmail.com>
2015-06-25 22:08:58 +0530

    remove 'categories' + show count of solved/total

--- markup.js
+++ markup.js
@@ -16,11 +16,13 @@ function isSuccessRateBelow80(rate) {
     return false;
 }
 
-function markIfSolved(problem, iconCell, matchTypeCell, divOneRateCell, divTwoRateCell) {
+function markIfSolved(count, countCell, problem, iconCell, matchTypeCell, divOneRateCell, divTwoRateCell) {
     chrome.storage.sync.get(problem, function (value) {
         if (value && value[problem]) {
             var imageURL = chrome.extension.getURL("green_tick.png");
             iconCell.innerHTML = '<img src="' + imageURL + '" /> ';
+            count.solved++;
+            count.total++;
         } else {
             var matchType = matchTypeCell.textContent.trim();
             var divOneRate = divOneRateCell.textContent.trim();
@@ -28,15 +30,19 @@ function markIfSolved(problem, iconCell, matchTypeCell, divOneRateCell, divTwoRa
             if (!isSRM(matchType) || isSuccessRateBelow80(divOneRate) || isSuccessRateBelow80(divTwoRate)) {
                 var imageURL = chrome.extension.getURL("question_mark.png");
                 iconCell.innerHTML = '<img src="' + imageURL + '" /> ';
+                count.total++;
             }
         }
+        countCell.innerHTML = '<div style="color: #FFFFFF; font-size: 12px">' + count.solved.toString() + '/' + count.total.toString() + '</div>';
     });
 }
 
 if (document.getElementsByClassName('paddingTable2').length > 0) {
     var table = document.getElementsByClassName("paddingTable2")[2];
+    var count = {solved:0, total:0};
     for (i = 3; i < table.rows.length - 6; i++) {
         var problem = table.rows[i].cells[1].textContent.trim();
-        markIfSolved(problem, table.rows[i].cells[0], table.rows[i].cells[2], table.rows[i].cells[7], table.rows[i].cells[9]);
+        table.rows[i].cells[5].innerHTML = '-';
+        markIfSolved(count, table.rows[1].cells[0], problem, table.rows[i].cells[0], table.rows[i].cells[2], table.rows[i].cells[7], table.rows[i].cells[9]);
     }
 }
